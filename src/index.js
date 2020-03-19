249eac420860ad2b404addb693fbc16e042e80ff
s14k51 <s14k51@gmail.com>
2020-03-19 00:36:40 -0500

    feat: add args

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const str2 = "hello2";
 function sayX() {
   const x = 1;
 
-  return isBoolean(x);
+  return isBoolean(x, 2);
 }
 
 function sayY() {
   const y = 1;
 
-  return isBoolean(y);
+  return isBoolean(y, 3);
 }
