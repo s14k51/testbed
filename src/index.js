9a67a402b0a8b5e4e5338eded1bdb6892445f91a
s14k51 <s14k51@gmail.com>
2022-09-11 21:25:22 -0500

    feat: add nested control flow block

--- src/index.js
+++ src/index.js
@@ -1,28 +1,21 @@
 const isBoolean = require("lodash/isBoolean");
 
-const str = "hello",
-  str2 = "hello2";
+const str = "hello";
 
 function sayX() {
   const x = 1;
   const y = 2;
   const z = 5;
 
+  if (true) {
+    if (true) {
+      if (true) {
+        if (true) {
+          console.log("ello");
+        }
+      }
+    }
+  }
+
   return isBoolean(x, 2);
-}
-
-function sayY() {
-  const x = 1;
-  const y = 2;
-  const z = 5;
-
-  return isBoolean(y, 3);
-}
-
-function sayY() {
-  const x = 1;
-  const y = 2;
-  const z = 5;
-
-  return isBoolean(y, 3);
 }
