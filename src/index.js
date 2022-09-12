42a26f4163535afdc82e87f7609ce1879d883863
s14k51 <s14k51@gmail.com>
2022-09-11 21:34:51 -0500

    feat: add one conditional check

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ function sayX() {
   const z = 5;
 
   for (const x of [1, 2, 3]) {
+    // 1
     console.log(x);
   }
 
-  if (true) {
+  if (x && y) {
+    // 2
     if (true) {
+      // 2
       if (true) {
+        // 3
         if (true) {
+          // 4
           console.log("ello");
         }
       }
